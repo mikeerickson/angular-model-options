1f6aca1d3d484f2ba1c10b09b7d9f073b5fd24a1
Mike Erickson <codedungeon@gmail.com>
2016-02-28 15:16:51 -0800

    Initial Commit

---
+++ public/js/app.constants.js
@@ -0,0 +1,10 @@
+// app.constants.js
+// =============================================================================
+
+var APP_DATA   = {
+	'APP_NAME': 'angular-model-options',
+	'API_BASE_URL': 'http://localhost:3000'
+};
+
+angular.module('app')
+	.constant('APP_DATA', APP_DATA);
---
+++ public/js/app.module.js
@@ -0,0 +1,4 @@
+angular.module('app', ['btorfs.multiselect'])
+ .run(function(){
+	 console.log('Hello World From MyApp');
+ });
